5584dccbc1d02004e6f72d790a75462cfb6f3049
GauravGS03 <88388239+GauravGS03@users.noreply.github.com>
2021-08-03 21:09:34 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,15 @@
+import random
 def primary():
   print("Hello World.")
   
   
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  last = 13
+  rnd = random.rndint(0,last)
+  print(quotes[rnd])
 
 if __name__== "__primary__":
   primary()
